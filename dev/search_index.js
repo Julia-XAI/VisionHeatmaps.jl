9c4e32e794ca0d9867c6c907918c058b06146abb
Documenter.jl <documenter@juliadocs.github.io>
2023-10-05 15:09:09 +0000

    build based on 7e5af44

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VisionHeatmaps","category":"page"},{"location":"#VisionHeatmaps","page":"Home","title":"VisionHeatmaps","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VisionHeatmaps.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VisionHeatmaps]","category":"page"}]
+}
