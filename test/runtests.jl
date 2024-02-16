3414bf7927651d3dcc7653b68ae1973273d4ea72
Adrian Hill <adrian.hill@mailbox.org>
2024-02-16 22:22:39 +0100

    Test code formatting

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,16 @@ using ColorSchemes
 using Test
 using ReferenceTests
 using Aqua
+using JuliaFormatter
 
 @testset "VisionHeatmaps.jl" begin
     @testset "Aqua.jl" begin
         @info "Running Aqua.jl's auto quality assurance tests. These might print warnings from dependencies."
         Aqua.test_all(VisionHeatmaps)
+    end
+    @testset "JuliaFormatter.jl" begin
+        @info "Running JuliaFormatter's code formatting tests."
+        @test format(VisionHeatmaps; verbose=false, overwrite=false)
     end
     @testset "Heatmap" begin
         @info "Testing heatmaps..."
