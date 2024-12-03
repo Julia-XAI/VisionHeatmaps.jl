33996169920b148a960543599dde1052e4aae0b2
adrhill <adrian.hill@mailbox.org>
2024-12-03 19:16:53 +0100

    Update tests

--- test/test_xai_presets.jl
+++ test/test_xai_presets.jl
@@ -1,7 +1,9 @@
-using XAIBase
+using VisionHeatmaps
+using ImageCore
 using Test
+using ReferenceTests
 
-using XAIBase: AbstractXAIMethod
+using XAIBase: Explanation, AbstractXAIMethod, AbstractOutputSelector
 import XAIBase: call_analyzer
 
 struct DummyAnalyzer <: AbstractXAIMethod end
@@ -13,7 +15,7 @@ function call_analyzer(
     batchsize = size(input)[end]
     v = reshape(output[output_selection], :, batchsize)
     val = input .* v
-    return Explanation(val, input, output, output_selection, :Dummy, :attribution)
+    return Explanation(val, input, output, output_selection, :Dummy, :sensitivity)
 end
 
 shape = (2, 2, 3, 1)
@@ -24,35 +26,37 @@ img = [RGB(1, 0, 0) RGB(0, 1, 0); RGB(0, 0, 1) RGB(1, 1, 1)]
 @testset "Heatmapping presets" begin
     expl = Explanation(val, input, output, output_selection, :DummyAnalyzer, :attribution)
     h = heatmap(expl)
-    @test_reference "references/seismic_sum_centered.txt" h
+    @test_reference "references/attribution.txt" h
     expl = Explanation(val, input, output, output_selection, :DummyAnalyzer, :sensitivity)
     h = heatmap(expl)
-    @test_reference "references/grays_norm_extrema.txt" h
+    @test_reference "references/sensitivity.txt" h
     expl = Explanation(val, input, output, output_selection, :DummyAnalyzer, :cam)
     h = heatmap(expl)
-    @test_reference "references/jet_sum_extrema.txt" h
+    @test_reference "references/cam.txt" h
 end
 
 @testset "Overlay presets" begin
     shape = (2, 2, 3, 1)
-    val = output = reshape(collect(Float32, 1:prod(shape)), shape)
+    val = output = input = reshape(collect(Float32, 1:prod(shape)), shape)
     output_selection = [[CartesianIndex(1, 2)]] # irrelevant
 
     expl = Explanation(val, input, output, output_selection, :DummyAnalyzer, :attribution)
     ho = heatmap_overlay(expl, img)
-    @test_reference "references/overlay/seismic_sum_centered.txt" ho
+    @test_reference "references/overlay/attribution.txt" ho
     expl = Explanation(val, input, output, output_selection, :DummyAnalyzer, :sensitivity)
     ho = heatmap_overlay(expl, img)
-    @test_reference "references/overlay/grays_norm_extrema.txt" ho
+    @test_reference "references/overlay/sensitivity.txt" ho
     expl = Explanation(val, input, output, output_selection, :DummyAnalyzer, :cam)
     ho = heatmap_overlay(expl, img)
-    @test_reference "references/overlay/jet_sum_extrema.txt" ho
+    @test_reference "references/overlay/cam.txt" ho
 end
 
 @testset "Batched input" begin
-    val = output = reshape(1:(2^4), 2, 2, 2, 2)
+    val = output = input = reshape(1.0:(2.0^4), 2, 2, 2, 2)
     output_selection = [CartesianIndex(1, 2), CartesianIndex(3, 4)] # irrelevant
-    expl_batch = Explanation(val, input, output, output_selection, :LRP, :attribution)
+    expl_batch = Explanation(
+        val, input, output, output_selection, :DummyAnalyzer, :sensitivity
+    )
 
     h1 = heatmap(expl_batch)
     h2 = heatmap(expl_batch; process_batch=true)
